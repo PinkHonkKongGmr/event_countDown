927271423900037256f808bfeaf9ae6341a5cac7
Vladimir Nickolaev <exlnted@gmail.com>
2020-05-06 10:52:52 +0300

    layout works

--- src/domconstructor.js
+++ src/domconstructor.js
@@ -3,27 +3,37 @@ import Year from './year';
 
 export const generateSelectorBlock = function () {
 	const yearInstance = new Year().year;
+
 	const wrapper = document.createElement('div');
+	wrapper.classList.add('form-row', 'form-group');
+
 	const startBtn = document.createElement('button');
 	startBtn.classList.add('btn', 'btn-primary');
-
-	const yearInput = document.createElement('input');
-	yearInput.classList.add('form-control');
-	yearInput.setAttribute('type', 'number');
-	yearInput.value = yearInstance.year;
+	startBtn.textContent = 'Запуск';
 
 	const wait = document.createElement('div');
 
 	const selectDays = new selectActions(generateSelector('disabled'));
 	const selectMounths = new selectActions(generateSelector('enabled'));
 
-	const dayWrapper = document.createElement('div').appendChild(selectDays.getInstance());
+	const dayWrapper = document.createElement('div');
 	dayWrapper.classList.add('form-group', 'col-md-1');
+	dayWrapper.appendChild(selectDays.getInstance());
 
-	const mounthWrapper = document.createElement('div').appendChild(selectMounths.getInstance());
+	const mounthWrapper = document.createElement('div');
 	mounthWrapper.classList.add('form-group', 'col-md-8');
-	wrapper.classList.add('form-group');
-	[dayWrapper, mounthWrapper, yearInput, startBtn, wait].forEach((child) => wrapper.appendChild(child));
+	mounthWrapper.appendChild(selectMounths.getInstance());
+
+	const yearInput = document.createElement('input');
+	yearInput.classList.add('form-control');
+	yearInput.setAttribute('type', 'number');
+	yearInput.value = yearInstance.year;
+
+	const yearWrapper = document.createElement('div');
+	yearWrapper.classList.add('form-group', 'col-md-3');
+	yearWrapper.appendChild(yearInput);
+
+	[dayWrapper, mounthWrapper, yearWrapper, startBtn, wait].forEach((child) => wrapper.appendChild(child));
 
 	return { yearInstance, selectDays, selectMounths, yearInput, startBtn, wait, wrapper };
 };
