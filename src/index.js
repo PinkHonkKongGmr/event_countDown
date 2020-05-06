17346cc5d10c3c3825aa6f7811d19e86427217f5
Vladimir Nickolaev <exlnted@gmail.com>
2020-05-06 14:47:38 +0300

    finish with add-delete mechanism

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import EventBlock from './eventblock';
 
 const root = document.getElementById('root');
+const addBtn = document.getElementById('add');
 
-function deleteBlock(block) {
-	root.removeChild(block);
-}
+const deleteBlock = (block) => root.removeChild(block);
 
-const eb = new EventBlock();
-const eb2 = new EventBlock();
-const newel = eb.createBlock();
-const newel2 = eb2.createBlock();
-root.appendChild(newel);
-root.appendChild(newel2);
-
-const deleteBlockBind1 = deleteBlock.bind(null, newel);
-const deleteBlockBind2 = deleteBlock.bind(null, newel2);
-eb.deleteBlock(deleteBlockBind1);
-eb2.deleteBlock(deleteBlockBind2);
+addBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	const eventBlock = new EventBlock();
+	const newBlock = eventBlock.createBlock();
+	root.appendChild(newBlock);
+	const deleteBlockBind = deleteBlock.bind(null, newBlock);
+	eventBlock.deleteBlock(deleteBlockBind);
+});
